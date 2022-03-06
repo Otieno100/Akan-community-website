4d5689cab63ab417d3fe3f7a7f520ddfeda0b5e9
Otieno100 <brian.masafu@student.moringaschool.com>
2022-03-06 12:46:17 +0300

    added js code;added more javascript code to select the name of the individual

--- script.js
+++ script.js
@@ -43,7 +43,7 @@
 
 
 
-// alert("Hello! This page has some Carrousel to Enjoy")
+// // alert("Hello! This page has some Carrousel to Enjoy")
 
 
 
@@ -71,7 +71,7 @@
 //   }
   
 //   if((MM<= 0) || (MM > 12) ) {
-//     // invalid month reurn
+//     // invalid month return
 //   }
 
 //   let dayOfWeekNumber =( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
@@ -139,24 +139,41 @@
 // }
 
 
-// var value = document.forms.gender.value
-// var submit = document.getElementById ("submit");
-// submit.onclick = ()=>{
-  // alert("Hello world")}
 
-  var male = document.getElementById("male").value
-  if (male==="Male"){
-    alert ("your selection is male");
-  }
+// function getInputValue() {
+//   // Selecting the input element and get its value 
+//   let inputVal = document.getElementsById("pick")[0].value;
+//   // Displaying the value
+//   alert(inputValue);
+// }
 
 
-// else if (value==="female"){
-  // alert ("you have selected Female")
-}
-  // else{
 
-    // alert("select gender")
-  // }
+
+
+
+
+
+
+
+// // // var value = document.forms.gender.value
+// // // var submit = document.getElementById ("submit");
+// // // submit.onclick = ()=>{
+// //   // alert("Hello world")}
+
+// //   var male = document.getElementById("male").value
+// //   if (male==="Male"){
+// //     alert ("your selection is male");
+// //   }
+
+
+// // // else if (value==="female"){
+// //   // alert ("you have selected Female")
+// // // }
+// //   // else{
+
+// //     // alert("select gender")
+// //   // }
 
 
 
@@ -164,3 +181,70 @@
 
 
 
+function submitDate() {
+  let date = document.getElementById('date').value;
+  let male = document.getElementById('male').checked
+  let female = document.getElementById('female').checked
+  let sex = 'male'
+  if (female) {
+      sex = 'female'
+  }
+  if (male) {
+      sex = 'male'
+  }
+  let day = getDay(date)
+  let dayOfWeek = getDayOfWeek(day)
+  let name = getTheName(dayOfWeek, sex)
+  document.getElementById("name").innerHTML = name
+}
+
+function getDay(date) {
+  let cc = date.substr(0, 2)
+  let yy = date.substr(0, 2);
+  let mm = date.substr(5, 2);
+  let dd = date.substr(8, 2)
+  let yearLength = yy.length
+  if (dd > 31) {
+      alert("Invalid date")
+  } else if (mm > 12) {
+      alert("Invalid month")
+  } else if (yearLength > 4) {
+      alert("Invalid year")
+  }
+  let d = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
+  return Math.round(d)
+}
+
+function getDayOfWeek(day) {
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  return days[day]
+}
+
+function getTheName(dayOfweek, sex) {
+  let female = {
+      Sunday: 'Akosua',
+      Monday: 'Adwoa',
+      Tuesday: 'Abenaa',
+      Wednesday: 'Akua',
+      Thursday: 'Yaa',
+      Friday: 'Afua',
+      Saturday: 'Amaa'
+  }
+  let male = {
+      Sunday: 'Kwasi',
+      Monday: 'Kwadwo',
+      Tuesday: 'Kwabenaa',
+      Wednesday: 'Kwaku',
+      Thursday: 'Yaw',
+      Friday: 'Kofi',
+      Saturday: 'Kwame'
+  }
+  if (sex === 'male') {
+      return male[dayOfweek]
+  }
+  return female[dayOfweek]
+}
+
+let day = getDayOfWeek(date)
+let dayOfWeek = getDayOfWeek(day)
+document.getElementById("name").innerHTML=dayOfWeek
